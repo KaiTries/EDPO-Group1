faecec7f38d100570c534db99543b266c9dd7221
KaiTries <112092835+KaiTries@users.noreply.github.com>
2024-02-26 11:00:29 +0100

    Finished experiment 2

---
+++ Exercise_1/Experiment_2/producer/src/main/java/com/example/Producer.java
@@ -0,0 +1,36 @@
+package com.example;
+
+import com.google.common.io.Resources;
+import org.apache.kafka.clients.producer.KafkaProducer;
+import org.apache.kafka.clients.producer.ProducerRecord;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.Properties;
+
+public class Producer {
+
+    public static void main(String[] args) throws IOException {
+        KafkaProducer<String, String> producer;
+        try (InputStream props = Resources.getResource("producer.properties").openStream()) {
+            Properties properties = new Properties();
+            properties.load(props);
+            producer = new KafkaProducer<>(properties);
+        }
+
+        try {
+            for (int i = 0; i < 100000; i++) {
+                Thread.sleep(1000);
+                producer.send(new ProducerRecord<String, String>(
+                      "user-events", // topic
+                      "user_id_" + i, // key
+                      "some_value_" + System.nanoTime())); // value
+            }
+        } catch (Throwable throwable) {
+            System.out.println(throwable.getStackTrace());
+        } finally {
+            producer.close();
+        }
+
+    }
+}
