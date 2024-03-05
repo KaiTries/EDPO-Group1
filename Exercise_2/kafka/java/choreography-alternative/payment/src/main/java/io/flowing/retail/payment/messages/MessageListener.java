5e48808b850161a08c4999e6721adc0176724331
KaiTries <112092835+KaiTries@users.noreply.github.com>
2024-03-05 12:02:27 +0100

    added GoodsNotFetched Event to PaymentService, correctly handles refund

--- Exercise_2/kafka/java/choreography-alternative/payment/src/main/java/io/flowing/retail/payment/messages/MessageListener.java
+++ Exercise_2/kafka/java/choreography-alternative/payment/src/main/java/io/flowing/retail/payment/messages/MessageListener.java
@@ -30,13 +30,12 @@ public class MessageListener {
   @Transactional
   @KafkaListener(id = "payment", topics = MessageSender.TOPIC_NAME)
   public void orderPlaced(String messageJson, @Header("type") String messageType) throws Exception {
+    // Note that we now have to read order data from this message!
+    // Bad smell 1 (reading some event instead of dedicated command)
+    JsonNode message = objectMapper.readTree(messageJson);
+    ObjectNode payload = (ObjectNode) message.get("data");
+    String orderId = payload.get("orderId").asText();
     if ("OrderPlacedEvent".equals(messageType)) {
-// Note that we now have to read order data from this message!
-      // Bad smell 1 (reading some event instead of dedicated command)
-      JsonNode message = objectMapper.readTree(messageJson);
-      ObjectNode payload = (ObjectNode) message.get("data");
-
-      String orderId = payload.get("orderId").asText();
       if (orderId == null) {
         // We do not yet have an order id - as the responsibility who creates that is unclear
         // Bad smell 2 (order context missing)
@@ -45,11 +44,8 @@ public class MessageListener {
         orderId = UUID.randomUUID().toString();
         payload.put("orderId", orderId);
       }
-      // the totalSum needs to be calculated by the checkout in this case - responsibility unclear
-      // as this is not done we have to calculate it here - which means we have to learn to much about orders!
-      // Bad smell 3 (order context missing)
-      long amount = payload.get("items").iterator().next().get("amount").asLong();
-      //long amount = payload.get("totalSum").asLong();
+
+      long amount = payload.get("totalSum").asLong();
 
       String paymentId = paymentService.createPayment(orderId, amount);
 
@@ -61,6 +57,23 @@ public class MessageListener {
       messageSender.send( //
               new Message<JsonNode>( //
                       "PaymentReceivedEvent", //
+                      message.get("traceid").asText(), //
+                      message.get("data")));
+    }
+    if ("GoodsNotFetchedEvent".equals(messageType)) {
+
+      long amount = payload.get("totalSum").asLong();
+
+      String refundId = paymentService.refundPayment(orderId, amount);
+
+      // Note that we need to pass along the whole order object
+      // Maybe with additional data we have
+      // Bad smell 4 (data flow passing through - data might grow big and most data is not needed for payment)
+      payload.put("refundId", refundId);
+
+      messageSender.send(
+              new Message<JsonNode>( //
+                      "PaymentRefundedEvent", //
                       message.get("traceid").asText(), //
                       message.get("data")));
     }
